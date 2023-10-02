bffdc70e2f3bad5f418a43258425b203306f9c80
sshovkov <sophiashovkovy@gmail.com>
2023-10-01 22:39:06 -0700

    v2.0.2

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="fractal_noise",
     packages=find_packages(include=["fractal_noise"]),
-    version="2.0.1",
+    version="2.0.2",
     description="Python implementation for Fractal Noise, ideal for creating realistic terrain, cloud-like textures, and intricate visual effects.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Sophia Shovkovy",
     author_email="sophiashovkovy@gmail.com",
     install_requires=["numpy", "perlin-noise"],
